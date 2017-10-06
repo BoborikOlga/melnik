0d954e4e5105a1886e1576e86573d8c1f7a47450
Mikhail Skirukha <mov1841@gmail.com>
2017-10-06 14:40:44 +0300

    Make template for lab3

---
+++ 3/Models_03/Models_03/MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Models_03
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SecondPiTextBox = new System.Windows.Forms.TextBox();
+            this.TicksTextBox = new System.Windows.Forms.TextBox();
+            this.FirstPiTextBox = new System.Windows.Forms.TextBox();
+            this.PTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ATextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.WcTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LcTextBox = new System.Windows.Forms.TextBox();
+            this.ImitateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SecondPiTextBox
+            // 
+            this.SecondPiTextBox.Location = new System.Drawing.Point(277, 35);
+            this.SecondPiTextBox.Name = "SecondPiTextBox";
+            this.SecondPiTextBox.Size = new System.Drawing.Size(83, 22);
+            this.SecondPiTextBox.TabIndex = 0;
+            // 
+            // TicksTextBox
+            // 
+            this.TicksTextBox.Location = new System.Drawing.Point(408, 35);
+            this.TicksTextBox.Name = "TicksTextBox";
+            this.TicksTextBox.Size = new System.Drawing.Size(83, 22);
+            this.TicksTextBox.TabIndex = 1;
+            // 
+            // FirstPiTextBox
+            // 
+            this.FirstPiTextBox.Location = new System.Drawing.Point(152, 35);
+            this.FirstPiTextBox.Name = "FirstPiTextBox";
+            this.FirstPiTextBox.Size = new System.Drawing.Size(83, 22);
+            this.FirstPiTextBox.TabIndex = 2;
+            // 
+            // PTextBox
+            // 
+            this.PTextBox.Location = new System.Drawing.Point(23, 35);
+            this.PTextBox.Name = "PTextBox";
+            this.PTextBox.Size = new System.Drawing.Size(83, 22);
+            this.PTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "p:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pi1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pi2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ticks:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "A:";
+            // 
+            // ATextBox
+            // 
+            this.ATextBox.Location = new System.Drawing.Point(23, 122);
+            this.ATextBox.Name = "ATextBox";
+            this.ATextBox.Size = new System.Drawing.Size(83, 22);
+            this.ATextBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(149, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Wc:";
+            // 
+            // WcTextBox
+            // 
+            this.WcTextBox.Location = new System.Drawing.Point(152, 122);
+            this.WcTextBox.Name = "WcTextBox";
+            this.WcTextBox.Size = new System.Drawing.Size(83, 22);
+            this.WcTextBox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(274, 102);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 17);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Lc:";
+            // 
+            // LcTextBox
+            // 
+            this.LcTextBox.Location = new System.Drawing.Point(277, 122);
+            this.LcTextBox.Name = "LcTextBox";
+            this.LcTextBox.Size = new System.Drawing.Size(83, 22);
+            this.LcTextBox.TabIndex = 12;
+            // 
+            // ImitateButton
+            // 
+            this.ImitateButton.Location = new System.Drawing.Point(23, 191);
+            this.ImitateButton.Name = "ImitateButton";
+            this.ImitateButton.Size = new System.Drawing.Size(83, 23);
+            this.ImitateButton.TabIndex = 14;
+            this.ImitateButton.Text = "Imitate";
+            this.ImitateButton.UseVisualStyleBackColor = true;
+            this.ImitateButton.Click += new System.EventHandler(this.ImitateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 234);
+            this.Controls.Add(this.ImitateButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LcTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.WcTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ATextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PTextBox);
+            this.Controls.Add(this.FirstPiTextBox);
+            this.Controls.Add(this.TicksTextBox);
+            this.Controls.Add(this.SecondPiTextBox);
+            this.Name = "MainForm";
+            this.Text = "Models 3-4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SecondPiTextBox;
+        private System.Windows.Forms.TextBox TicksTextBox;
+        private System.Windows.Forms.TextBox FirstPiTextBox;
+        private System.Windows.Forms.TextBox PTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ATextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox WcTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox LcTextBox;
+        private System.Windows.Forms.Button ImitateButton;
+    }
+}
+
